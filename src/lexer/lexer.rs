a036d9697694364ebf8d653729689cc83bc82786
kkpagaev <kkpagaev@gmail.com>
2023-11-08 05:31:18 +0200

    feat(lexer): basic lexer implemented

---
+++ src/lexer/lexer.rs
@@ -0,0 +1,150 @@
+use std::str::Chars;
+use std::iter::Peekable;
+
+use super::Token;
+
+pub struct Lexer<'a> {
+    input: Peekable<Chars<'a>>,
+
+    ch: u8,
+}
+
+#[allow(dead_code)]
+impl<'a> Lexer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        let mut lexer = Self {
+            input: input.chars().peekable(),
+            ch: 0,
+        };
+        lexer.next_char();
+        lexer
+    }
+
+    fn next_char(&mut self) {
+        self.ch = match self.input.peek(){
+            Some(ch) => *ch as u8,
+            None => 0
+        };
+
+        self.input.next();
+    }
+
+    fn peek(&mut self) -> u8 {
+        match self.input.peek(){
+            Some(ch) => *ch as u8,
+            None => 0
+        }
+    }
+
+    pub fn next(&mut self) -> Token {
+        self.skip_whitespace();
+
+        match self.ch {
+            b'a'..=b'z' | b'A'..=b'Z' | b'_' => {
+                self.consume_ident()
+            }
+            b'.' => {
+                self.next_char();
+                Token::Dot
+            }
+            b':' => {
+                self.next_char();
+                Token::Colon
+            }
+            b'0'..=b'9' => {
+                self.consume_int()
+            }
+            b',' => {
+                self.next_char();
+                Token::Comma
+            }
+            b'(' => {
+                self.next_char();
+                Token::LRound
+            }
+            b')' => {
+                self.next_char();
+                Token::RRound
+            }
+            b'{' => {
+                self.next_char();
+                Token::LCurly
+            }
+            b'}' => {
+                self.next_char();
+                Token::LCurly
+            }
+            b'[' => {
+                self.next_char();
+                Token::LSquare
+            }
+            b']' => {
+                self.next_char();
+                Token::RSquare
+            }
+            b'"' | b'\'' => {
+                self.consume_string()
+            }
+            0 => Token::Eof,
+            _ => {
+                self.next_char();
+                Token::Illegal
+            }
+        }
+    }
+
+    fn consume_string(&mut self) -> Token {
+        let mut value = String::new();
+        let quote_type = self.ch;
+        let mut next_skip = false;
+
+        self.next_char();
+
+        while self.ch != quote_type || next_skip {
+            next_skip = self.ch == b'\\';
+            value.push(self.ch as char);
+            self.next_char();
+        }
+
+        self.next_char();
+
+        Token::Ident(value)
+    }
+
+
+    fn consume_ident(&mut self) -> Token {
+        let mut value = String::new();
+
+        while self.ch.is_ascii_alphanumeric() || self.ch == b'_' {
+            value.push(self.ch as char);
+            self.next_char();
+        }
+
+        Token::Ident(value)
+    }
+
+
+    fn skip_whitespace(&mut self) {
+        loop {
+            match self.ch {
+                b' ' | b'\t' | b'\n' | b'\r' => {
+                    self.next_char();
+                }
+                _ => {
+                    break;
+                }
+            }
+        }
+    }
+
+    fn consume_int(&mut self) -> Token {
+        let mut value = String::new();
+
+        while self.ch.is_ascii_digit() || self.ch == b'.' {
+            value.push(self.ch as char);
+            self.next_char();
+        }
+
+        Token::Int(value)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-use rust_ts_json_compiler::*;
+use rust_ts_json_compiler::lexer::{Lexer, Token};
 
 pub fn main() {
-    println!("{:?}", list_parser::list("[1,1,2,3,5,8]"));
+    let schema = "z.object({
+  id: z.coerce.number()
+})";
+    let mut lexel = Lexer::new(schema);
+    let mut tokens = Vec::new();
+
+    loop {
+        let token = lexel.next();
+        if token == Token::Eof {
+            break;
+        }
+
+        tokens.push(token);
+    }
+
+    println!("{:?}", tokens);
 }
